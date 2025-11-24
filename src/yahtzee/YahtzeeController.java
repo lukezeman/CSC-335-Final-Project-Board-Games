53ee9414e38fc4387c06bd79df8576b734ce1055
Luke Zeman <lpz88@outlook.com>
2025-11-23 18:54:02 -0700

    completed yahtzee controller

--- src/yahtzee/YahtzeeController.java
+++ src/yahtzee/YahtzeeController.java
@@ -1,5 +1,49 @@
 package yahtzee;
 
 public class YahtzeeController {
-
+	private YahtzeeModel model;
+	
+	public YahtzeeController(YahtzeeModel model) {
+		this.model = model;
+	}
+	
+	protected int[] rollDice() {
+		return model.rollDice();
+	}
+	
+	protected void recordScore(YahtzeeCategory category, int[] dice) {
+		model.recordScore(category, dice);
+	}
+	
+	protected boolean recordBonusYahtzee(int[] dice) {
+		return model.recordBonusYahtzee(dice);
+	}
+	
+	protected boolean isGameOver() {
+		return model.isGameOver();
+	}
+	
+	protected YahtzeePlayer getCurrentPlayer() {
+		return model.getCurrentPlayer();
+	}
+	
+	protected int getRollsRemaining() {
+		return model.getRollsRemaining();
+	}
+	
+	protected YahtzeePlayer getWinner() {
+		return model.getWinner();
+	}
+	
+	protected YahtzeeScorecard getScorecard() {
+		return model.getScorecard();
+	}
+	
+	protected YahtzeePlayer getPlayer1() {
+		return model.getPlayer1();
+	}
+	
+	protected YahtzeePlayer getPlayer2() {
+		return model.getPlayer2();
+	}
 }
