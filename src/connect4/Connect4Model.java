d447d9ab3065a0dfa8910b1cc0b92557c58a1c30
achillessoto <143756119+achillessoto@users.noreply.github.com>
2025-11-26 20:16:03 -0700

    Connect4 model tested and complete

--- src/connect4/Connect4Model.java
+++ src/connect4/Connect4Model.java
@@ -1,3 +1,10 @@
+/**
+ * @author Achilles Soto
+ * Course: CSC 335
+ * File: Connect4Model.java
+ * This program handles all functionality of connect4, making 
+ * moves for both players, updating the game board accordingly.
+ */
 package connect4;
 
 import java.util.Observable;
@@ -8,24 +15,38 @@ public class Connect4Model extends Observable {
 	
 	public Connect4Model() {
 		board = new char[6][7];
-		board[5][6] = 'r';
-		playerTurn = 'r';
-		
-		for (char[] row : board) {
-			for (char col : row) {
-				if (col == '\0')System.out.print(".");
-				else System.out.println(col);
-			}
-			System.out.println();
-		}
+		playerTurn = 'y';
 	}
 	
+	/**
+	 * Retrieves the current Connect4 game
+	 * 
+	 * @return a char[][] which holds all player's pieces
+	 */
 	public char[][] getBoard(){
 		return board;
 	}
 	
+	public void setBoard(char[][] board){
+		this.board = board;
+	}
+	
+	/**
+	 * Retrieves which ever player's turn it is
+	 * 
+	 * @return a char which represents the player color
+	 */
 	public char getTurn() {
 		return playerTurn;
+	}
+	
+	/**
+	 * Sets a player's turn if the player doesn't want to start first
+	 * 
+	 * @param player - a char which represents the player color
+	 */
+	public void setTurn(char player) {
+		playerTurn = player;
 	}
 	
 	public boolean setTurn(int col, char player) {
@@ -35,29 +56,146 @@ public class Connect4Model extends Observable {
 		if (board[row][col] != '\0') return false;
 		
 		// Looks for a empty slot that has a slot under that isn't empty
-		while (board[row + 1][col] != '0') row ++;
+		while ((row + 1) <= 5 && board[row + 1][col] != '\0') row ++;
 		
 		board[row][col] = player;
 		
+		int[] pair = {row, col};
+		
 		setChanged();
-		notifyObservers(board);
+		notifyObservers(pair);
 		
 		return true;
 	}
 	
-	public boolean isGameOver(int row, int col) {		
-		if (isWinner(row, col)) return true;
+	/**
+	 * Checks if board is full of pieces or if a player has connected 4 pieces
+	 * 
+	 * @param row - an int which is the row a piece landed in
+	 * @param col - an int which is the column a piece was dropped in
+	 * @return an int, 1(Game over by winner), 0 (Not game over), -1 (Game over by draw).
+	 */
+	public int isGameOver(int row, int col) {	
 		
+		// Checks if theres a winner yet
+		if (isWinner(row, col)) return 1;
+		
+		// First row still has open slots
 		for (char item : board[row]) {
-			if (item == '\0') break;
+			if (item == '\0') return 0;
 		}	
+		
+		// Theres no open slots or winner so its a draw
+		return -1;
+	}
+	
+	/**
+	 * Checks if there's a winner from all possible directions for the last move
+	 * 
+	 * @param row - an int which is the row a piece landed in
+	 * @param col - an int which is the column a piece was dropped in
+	 * @return a boolean where true is the player has won and false no winner.
+	 */
+	public boolean isWinner(int row, int col) {
+		char player = board[row][col];
+		
+		if (checkDiagonal(row, col, player)) return true;
+		if (checkVertical(row, col, player)) return true;
+		if (checkHorizontal(row, col, player)) return true;
+
 		return false;
 	}
 	
-	public boolean isWinner(int row, int col) {
-		char player = board[row][col];
-		int[][] dirs = {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0},{1,1}};
+	/**
+	 * Checks if there are four connected pieces diagonally from a coordinate
+	 * 
+	 * @param row - an int which is the row a piece landed in
+	 * @param col - an int which is the column a piece was dropped in
+	 * @param player - a char which represents the player color
+	 * @return a boolean, true if there are at least four connected player pieces diagonally
+	 */
+	private boolean checkDiagonal(int row, int col, char player){
+		// Counts pieces from upper left and lower right of the coordinate
+		int diagDir1 = 1;
+		diagDir1 += countConnected(row, col, -1, -1, player);
+		diagDir1 += countConnected(row, col, 1, 1, player);
+		
+		// Checks if theres at least four connected
+		if (diagDir1 >= 4) return true;
+		
+		// Counts pieces from upper right to lower left of the coordinate
+		int diagDir2 = 1;
+		diagDir2 += countConnected(row, col, -1, 1, player);
+		diagDir2 += countConnected(row, col, 1, -1, player);
+		
+		// Checks if theres at least four connected
+		if (diagDir2 >= 4) return true;
 		
 		return false;
+	}
+	
+	/**
+	 * Checks if there are four connected pieces horizontally from a coordinate
+	 * 
+	 * @param row - an int which is the row a piece landed in
+	 * @param col - an int which is the column a piece was dropped in
+	 * @param player - a char which represents the player color
+	 * @return a boolean, true if there are at least four connected player pieces horizontally
+	 */
+	private boolean checkHorizontal(int row, int col, char player) {
+		// Counts pieces to the left and right of the coordinate
+		int count = 1;
+		count += countConnected(row, col, 0, -1, player);
+		count += countConnected(row, col, 0, 1, player);
+		
+		// Checks if theres at least four connected
+		if (count >= 4) return true;
+		
+		return false;
+	}
+	
+	/**
+	 * Checks if there are four connected pieces vertically from a coordinate
+	 * 
+	 * @param row - an int which is the row a piece landed in
+	 * @param col - an int which is the column a piece was dropped in
+	 * @param player - a char which represents the player color
+	 * @return a boolean, true if there are at least four connected player pieces vertically
+	 */
+	private boolean checkVertical(int row, int col, char player) {
+		// Counts pieces above and below the coordinate
+		int count = 1;
+		count += countConnected(row, col, -1, 0, player);
+		count += countConnected(row, col, 1, 0, player);
+	
+		// Checks if theres at least four connected
+		if (count >= 4) return true;
+		
+		return false;
+	}
+	
+	/**
+	 * Checks if there are four of a players pieces in a row going a certain direction
+	 * 
+	 * @param row - an int which is the row a piece landed in
+	 * @param col - an int which is the column a piece was dropped in
+	 * @param xDir - an int which is the direction x is going to move towards
+	 * @param yDir - an int which is the direction y is going to move towards
+	 * @param player - a char which represents the player color
+	 * @return a int which is the count of the number of connected pieces in a direction
+	 */
+	private int countConnected(int row, int col, int xDir, int yDir, char player) {
+		int x = row + xDir;
+		int y = col + yDir;
+		int counter = 0;
+		
+		// Loops over pieces in a direction while staying inbounds
+		while ((x >= 0 && x <= 5 && y >= 0 && y <= 6) && board[x][y] == player) {
+			counter += 1;
+			x += xDir;
+			y += yDir;
+		}
+		
+		return counter;
 	}
 }
--- src/connect4/Connect4Test.java
+++ src/connect4/Connect4Test.java
@@ -7,9 +7,179 @@ import org.junit.jupiter.api.Test;
 class Connect4Test {
 
 	@Test
-	void testModel() {
+	void testModelSetTurn() {
 		Connect4Model model = new Connect4Model();
+		assertTrue(model.setTurn(0, 'r'));
+		model.setTurn(5, 'r');
+		
+		char[][] board = model.getBoard();
+		
+		assertTrue(board[5][6] == 'r');
+
+		model.setTurn(5, 'r');
+		model.setTurn(5, 'r');
+		model.setTurn(5, 'r');
+		model.setTurn(5, 'r');
+		assertFalse(model.setTurn(5, 'r'));
+
+	}
 	
+	@Test
+	void testModelGameOver() {
+		System.out.println("<------------------------->");
+
+		Connect4Model model = new Connect4Model();
+		
+		assertEquals(0 ,model.isGameOver(5, 6));
+
+		char[][] board = new char[6][7];
+		
+		char p = 'y';
+		int count = 0;
+		for (char[] row : board) {
+			for (int i = 0; i < row.length; i++) {
+				if (count == 2 && p == 'y') {
+					count = 0;
+					p = 'r';
+				}
+				else if (count == 2 && p == 'r') {
+					count = 0;
+					p = 'y';
+				}
+				row[i] = p;
+				count++;
+			}
+			count = 0;
+		}
+		
+		for (char[] row1 : board) {
+			for (char col2 : row1) {
+				if (col2 == '\0')System.out.print(".");
+				else System.out.print(col2);
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		model.setBoard(board);
+		assertEquals(-1 ,model.isGameOver(0, 0));
+		System.out.println("<------------------------->");
+
+	}
+	
+	@Test
+	void testModelDiagWin() {
+		System.out.println("<------------------------->");
+		Connect4Model model = new Connect4Model();
+		char[][] board = new char[6][7];
+		board[5][6] = 'r';
+		board[4][5] = 'r';
+		board[3][4] = 'r';
+		board[2][3] = 'r';
+		board[1][2] = 'r';
+		board[0][1] = 'r';
+		
+		for (char[] row1 : board) {
+			for (char col2 : row1) {
+				if (col2 == '\0')System.out.print(".");
+				else System.out.print(col2);
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+
+		
+		model.setBoard(board);
+		
+		System.out.println("Win D1 check");
+		assertEquals(1 ,model.isGameOver(4, 5));
+		
+		board = new char[6][7];
+		board[5][0] = 'r';
+		board[4][1] = 'r';
+		board[3][2] = 'r';
+		board[2][3] = 'r';
+		board[1][4] = 'r';
+		board[0][5] = 'r';
+		
+		for (char[] row1 : board) {
+			for (char col2 : row1) {
+				if (col2 == '\0')System.out.print(".");
+				else System.out.print(col2);
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		model.setBoard(board);
+		
+		System.out.println("Win D2 check");
+		assertEquals(1 ,model.isGameOver(4, 1));
+		System.out.println("<------------------------->");
+	}
+	
+	@Test
+	void testModelHoriWin() {
+		System.out.println("<------------------------->");
+		Connect4Model model = new Connect4Model();
+		char[][] board = new char[6][7];
+		board[5][6] = 'r';
+		board[5][5] = 'r';
+		board[5][4] = 'r';
+		board[5][3] = 'r';
+		board[5][2] = 'r';
+		
+		for (char[] row1 : board) {
+			for (char col2 : row1) {
+				if (col2 == '\0')System.out.print(".");
+				else System.out.print(col2);
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		model.setBoard(board);
+		
+		System.out.println("Win Hori check");
+		assertEquals(1 , model.isGameOver(5, 4));
+		System.out.println("<------------------------->");
+
+	}
+	
+	@Test
+	void testModelVertWin() {
+		System.out.println("<------------------------->");
+		Connect4Model model = new Connect4Model();
+		char[][] board = new char[6][7];
+		board[5][5] = 'r';
+		board[4][5] = 'r';
+		board[3][5] = 'r';
+		board[2][5] = 'r';
+		board[1][5] = 'r';
+		board[0][5] = 'r';
+		
+		for (char[] row1 : board) {
+			for (char col2 : row1) {
+				if (col2 == '\0')System.out.print(".");
+				else System.out.print(col2);
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+
+		
+		model.setBoard(board);
+		
+		System.out.println("Win Vert check");
+		assertEquals(1 ,model.isGameOver(4, 5));
+		
+		System.out.println("<------------------------->");
+
 	}
 
 }
